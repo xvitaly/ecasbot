7cf14ef50a69f6842c6ab3b9f16794a1e3bfa3fe
Vitaly Zaitsev <vitaly@easycoding.org>
2017-09-15 21:51:41 +0500

    Added more debug information.

--- src/ecasbot.py
+++ src/ecasbot.py
@@ -40,17 +40,17 @@ def runbot(key):
     def handle_join(message):
         if message.from_user.id not in blacklist:
             blacklist.append(message.from_user.id)
-        bot.reply_to(message, 'Приветствуем вас в нашем чате! Это тестовое оповещение на время тестов бота. Ваш ID записан в наш журнал.')
+        bot.reply_to(message, 'Приветствуем вас в нашем чате! Это тестовое оповещение на время тестов бота. Ваш ID записан в наш журнал. Не размещайте ссылок, иначе нам придётся вас заблокировать!')
 
     @bot.message_handler(func=lambda m: m.chat.type == "supergroup" and m.from_user.id in blacklist)
     def handle_msg(message):
         if match('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.text):
             try:
                 # Removing spam message and restricting user for 30 minutes...
-                bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date=time() + 30 * 60)
                 bot.delete_message(message.chat.id, message.message_id)
+                bot.restrict_chat_member(message.chat.id, message.from_user.id, until_date=time() + 30 * 60)
             except:
-                print('Обнаружено спам-сообщение, но у меня нет прав на его удаление и блокировку пользователя!')
+                print('Found spam message from %s, but I have no admin rights in this channel to delete it and restrict user.' % message.from_user.id)
 
     # Run bot forever...
     bot.polling(none_stop=True)

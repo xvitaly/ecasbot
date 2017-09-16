4c3f710dd9a9ece99b680e502f661e8aa3867b7f
Vitaly Zaitsev <vitaly@easycoding.org>
2017-09-17 00:52:40 +0500

    Added basic setup script.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# coding=utf-8
+
+from distutils.core import setup
+
+setup(
+    name='ecasbot',
+    version='0.1pre',
+    packages=['ecasbot'],
+    url='https://www.easycoding.org/projects/ecasbot',
+    license='GPLv3',
+    author='Vitaly Zaitsev',
+    author_email='vitaly@easycoding.org',
+    description='AntiSpam bot for Telegram by EasyCoding Team'
+)

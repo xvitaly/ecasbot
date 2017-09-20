da2d4dd2e12752b9c829134d5372c288fa470e8c
Vitaly Zaitsev <vitaly@easycoding.org>
2017-09-20 22:11:47 +0500

    Use private propeties for class.

--- ecasbot/__init__.py
+++ ecasbot/__init__.py
@@ -13,7 +13,7 @@ class ASBot:
         print('(%s) %s' % (datetime.fromtimestamp(time()).strftime('%d.%m.%Y %H:%M:%S'), msg))
 
     def msg_check(self, m):
-        return m.chat.type == 'supergroup' and m.from_user.id in self.blacklist
+        return m.chat.type == 'supergroup' and m.from_user.id in self.__blacklist
 
     def runbot(self):
         # Initialize command handlers...
@@ -25,8 +25,8 @@ class ASBot:
         @self.bot.message_handler(commands=['addme'])
         def handle_addme(message):
             if message.chat.type == "private":
-                if message.from_user.id not in self.blacklist:
-                    self.blacklist.append(message.from_user.id)
+                if message.from_user.id not in self.__blacklist:
+                    self.__blacklist.append(message.from_user.id)
                     self.bot.send_message(message.chat.id, self.__msgs['as_addsc'])
                 else:
                     self.bot.send_message(message.chat.id, self.__msgs['as_addex'])
@@ -34,8 +34,8 @@ class ASBot:
         @self.bot.message_handler(commands=['removeme'])
         def handle_removeme(message):
             if message.chat.type == "private":
-                if message.from_user.id in self.blacklist:
-                    self.blacklist.remove(message.from_user.id)
+                if message.from_user.id in self.__blacklist:
+                    self.__blacklist.remove(message.from_user.id)
                     self.bot.send_message(message.chat.id, self.__msgs['as_delsc'])
                 else:
                     self.bot.send_message(message.chat.id, self.__msgs['as_delex'])
@@ -48,8 +48,8 @@ class ASBot:
         @self.bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
         def handle_join(message):
             try:
-                if message.from_user.id not in self.blacklist:
-                    self.blacklist.append(message.from_user.id)
+                if message.from_user.id not in self.__blacklist:
+                    self.__blacklist.append(message.from_user.id)
                     self.bot.reply_to(message, self.__msgs['as_newsr'])
                     if self.__rest_new:
                         self.bot.restrict_chat_member(message.chat.id, message.from_user.id,
@@ -69,7 +69,7 @@ class ASBot:
                             # Removing spam message and restricting user for N minutes...
                             self.bot.delete_message(message.chat.id, message.message_id)
                             self.bot.restrict_chat_member(message.chat.id, message.from_user.id,
-                                                          until_date=time() + self.bantime)
+                                                          until_date=time() + self.__bantime)
             except Exception as ex:
                 self.log(self.__msgs['as_msgex'] % (message.from_user.id, ex))
 
@@ -79,8 +79,8 @@ class ASBot:
 
     def __init__(self, key):
         self.bot = TeleBot(key)
-        self.blacklist = []
-        self.bantime = 60 * 60 * 1
+        self.__blacklist = []
+        self.__bantime = 60 * 60 * 1
         self.__rest_new = False
         self.__rest_time = 60 * 60 * 24 * 7
         self.__msgs = {

39005160e40246dc854cf460ad02731447f73d93
Vitaly Zaitsev <vitaly@easycoding.org>
2018-08-09 16:48:43 +0200

    Use str.format() method to concatenate string including name and surname fields.

--- ecasbot/__init__.py
+++ ecasbot/__init__.py
@@ -55,7 +55,7 @@ class ASBot:
                     self.logger.exception(self.__msgs['as_restex'].format(message.from_user.id))
 
                 # Find and block chineese bots...
-                username = message.new_chat_member.first_name + message.new_chat_member.last_name if message.new_chat_member.last_name else message.new_chat_member.first_name
+                username = '{} {}'.format(message.new_chat_member.first_name, message.new_chat_member.last_name) if message.new_chat_member.last_name else message.new_chat_member.first_name
                 if re.search(self.settings.chkrgx, username, re.I | re.M | re.U):
                     try:
                         # Write user ID to log...

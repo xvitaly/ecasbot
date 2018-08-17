289dac3f73aba111475f2b6b8e3e020b70e17c11
Vitaly Zaitsev <vitaly@easycoding.org>
2018-08-17 19:25:06 +0200

    Removed some words from default blacklist.

--- ecasbot/settings.py
+++ ecasbot/settings.py
@@ -63,7 +63,7 @@ class Settings:
     def __create(self):
         self.__data = {'tgkey': '', 'chkrgx': '(.*VX.*QQ.+)', 'bantime': 60 * 60 * 24 * 3,
                        'admins': [], 'restent': ['url', 'text_link', 'mention'], 'maxname': 75,
-                       'stopwords': ['SEO', 'продвижение', 'Deleted']}
+                       'stopwords': ['SEO', 'Deleted']}
         dirname = os.path.dirname(self.__cfgfile)
         if not os.path.exists(dirname):
             os.makedirs(dirname)

32d0a010658e90e51a0c7c684c118e72bc8e0f79
Vitaly Zaitsev <vitaly@easycoding.org>
2018-09-20 22:18:46 +0500

    Increase score goal for messages from 50 to 100.

--- ecasbot/__init__.py
+++ ecasbot/__init__.py
@@ -56,7 +56,7 @@ class ASBot:
         return False
 
     def __check_message_spam(self, message) -> bool:
-        return self.__score_message(message) >= 50
+        return self.__score_message(message) >= 100
 
     def __score_user(self, fname, lname) -> int:
         # Setting default score to 0...

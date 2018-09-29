50dc21855713f6a0ead8d1342dc0e627c6c1eefe
Vitaly Zaitsev <vitaly@easycoding.org>
2018-09-29 22:46:49 +0500

    Specify return types for CheckUsername's methods.

--- ecasbot/validators.py
+++ ecasbot/validators.py
@@ -22,37 +22,37 @@ import re
 
 class CheckUsername:
     @classmethod
-    def __find_methods(cls, prefix):
+    def __find_methods(cls, prefix) -> set:
         print(cls.__dict__.keys())
         return {s for s in cls.__dict__.keys() if s.startswith(prefix)}
 
-    def check_chinese_bots(self):
+    def check_chinese_bots(self) -> int:
         # Find chinese bots and score them to +100...
         return 100 if re.search(self.__settings.chkrgx, self.__username, re.I | re.M | re.U) else 0
 
-    def check_with_url(self):
+    def check_with_url(self) -> int:
         # Score users with URLs in username...
         return 100 if re.search(self.__settings.urlrgx, self.__username, re.I | re.M | re.U) else 0
 
-    def check_restricted_words(self):
+    def check_restricted_words(self) -> int:
         # Score users with restricted words in username...
         return 100 if any(w in self.__username for w in self.__settings.stopwords) else 0
 
-    def check_too_long(self):
+    def check_too_long(self) -> int:
         # Score users with very long usernames...
         return 50 if len(self.__username) > self.__settings.maxname else 0
 
-    def check_hieroglyphs(self):
+    def check_hieroglyphs(self) -> int:
         # Score users with chinese hieroglyphs...
         return 50 if re.search('[\u4e00-\u9fff]+', self.__username, re.I | re.M | re.U) else 0
 
-    def get_score(self):
+    def get_score(self) -> int:
         score = 0
         for chk in self.__scorers:
             score += getattr(self, chk)()
         return score
 
-    def __init__(self, fname, lname, settings):
+    def __init__(self, fname, lname, settings) -> None:
         self.__username = '{} {}'.format(fname, lname) if lname else fname
         self.__settings = settings
         self.__scorers = self.__find_methods('check')

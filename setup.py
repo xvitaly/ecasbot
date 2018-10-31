45c166d2ebc3f4d2c62ae901b801d17584260ff9
Vitaly Zaitsev <vitaly@easycoding.org>
2018-10-31 22:16:30 +0500

    Bump version to 1.0.0.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecasbot',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
     package_dir={
         'ecasbot': 'ecasbot',

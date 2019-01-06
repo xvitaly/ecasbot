1df2565a5dec734f57b887ce117ecfb09a06158d
Vitaly Zaitsev <vitaly@easycoding.org>
2019-01-06 14:11:55 +0100

    Incremented version to 1.1.0.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecasbot',
-    version='1.0.0',
+    version='1.1.0',
     packages=find_packages(),
     package_dir={
         'ecasbot': 'ecasbot',

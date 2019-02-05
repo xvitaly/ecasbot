ea072240a7a02631ebb57ab11f8de7de848ae98b
Vitaly Zaitsev <vitaly@easycoding.org>
2019-02-05 11:11:09 +0100

    Method handle_unrestrict() will notify admins now.

--- ecasbot/__init__.py
+++ ecasbot/__init__.py
@@ -302,21 +302,22 @@ class ASBot:
                     self.bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id,
                                                   can_send_messages=True, can_send_media_messages=True,
                                                   can_send_other_messages=True, can_add_web_page_previews=True)
-                    self.__logger.warning(
-                        self.__msgs['as_aunres'].format(message.from_user.first_name, message.from_user.id,
-                                                        message.reply_to_message.from_user.first_name,
-                                                        message.reply_to_message.from_user.id, message.chat.id,
-                                                        message.chat.title))
+                    logmsg = self.__msgs['as_aunres'].format(message.from_user.first_name, message.from_user.id,
+                                                             message.reply_to_message.from_user.first_name,
+                                                             message.reply_to_message.from_user.id, message.chat.id,
+                                                             message.chat.title)
+                    self.__logger.warning(logmsg)
+                    self.__notify_admin(message, logmsg)
                 else:
                     unbanreq = ParamExtractor(message.text)
                     if unbanreq.index != -1:
                         userreq = self.bot.get_chat_member(message.chat.id, int(unbanreq.param))
                         self.bot.unban_chat_member(message.chat.id, userreq.user.id)
-                        self.__logger.warning(
-                            self.__msgs['as_aunban'].format(message.from_user.first_name, message.from_user.id,
-                                                            userreq.user.first_name, userreq.user.id, message.chat.id,
-                                                            message.chat.title))
-
+                        logmsg = self.__msgs['as_aunban'].format(message.from_user.first_name, message.from_user.id,
+                                                                 userreq.user.first_name, userreq.user.id,
+                                                                 message.chat.id, message.chat.title)
+                        self.__logger.warning(logmsg)
+                        self.__notify_admin(message, logmsg)
             except:
                 self.__logger.exception(self.__msgs['as_admerr'])
 

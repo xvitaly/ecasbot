xvitaly/ecasbot

commit 31746943e3cc431612bbe893f298e995d6dde6b4

Added SPDX preambles. Updated copyright year.
Closes #140. Closes #139.

packaging/windows/inno/locale/en/cm.isl
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 [CustomMessages]
 APIKeyPageCaption=API token
 APIKeyPageDescription=Telegram Bot API token page.

packaging/windows/inno/locale/ru/cm.isl
@@ -1,4 +1,8 @@
-﻿[CustomMessages]
+﻿# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+[CustomMessages]
 APIKeyPageCaption=Токен API
 APIKeyPageDescription=Ввод токена Telegram Bot API.
 APIKeyPageAdditionalText=Пожалуйста, введите на этой странице свой токен Telegram Bot API. Если у вас его нет, получить новый можно у бота @BotFather.

.appveyor.yml
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 version: '{build}'
 pull_requests:
   do_not_increment_build_number: true

.github/ISSUE_TEMPLATE/bug-report.yml
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 name: Bug report
 description: Create a new bug report ticket
 labels: [bug]

.github/ISSUE_TEMPLATE/config.yml
@@ -1,7 +1,5 @@
-#
-# SPDX-FileCopyrightText: 2011-2022 EasyCoding Team
+# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-#
 
 blank_issues_enabled: false

.github/ISSUE_TEMPLATE/feature-request.yml
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 name: Feature request
 description: Request a new feature
 labels: [request]

.github/ISSUE_TEMPLATE/question.yml
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 name: Question
 description: Ask a question about the project
 labels: [question]

.github/workflows/python.yaml
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 name: Python CI
 
 on:

commit 65a8a4c249be35208b1a500542785c0bf70bdcab

Fixed InnoSetup build.
Regression introduced in 31746943e3cc431612bbe893f298e995d6dde6b4.

packaging/windows/inno/locale/en/cm.isl
@@ -1,6 +1,6 @@
-# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
+; SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+;
+; SPDX-License-Identifier: GPL-3.0-or-later
 
 [CustomMessages]
 APIKeyPageCaption=API token

packaging/windows/inno/locale/ru/cm.isl
@@ -1,6 +1,6 @@
-﻿# SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
-#
-# SPDX-License-Identifier: GPL-3.0-or-later
+﻿; SPDX-FileCopyrightText: 2017-2022 EasyCoding Team
+;
+; SPDX-License-Identifier: GPL-3.0-or-later
 
 [CustomMessages]
 APIKeyPageCaption=Токен API
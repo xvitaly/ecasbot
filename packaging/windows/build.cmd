29f4a6c62fc05cc9961d97eb190f4527209d10cd
Vitaly Zaitsev <vitaly@easycoding.org>
2022-03-30 15:50:30 +0200

    Unified Microsoft Windows build script.
    Closes #138.

--- packaging/windows/build.cmd
+++ packaging/windows/build.cmd
@@ -10,6 +10,12 @@ set RELVER=152
 set GPGKEY=A989AAAA
 set PYTHONOPTIMIZE=1
 
+if [%CI_HASH%] == [] (
+    set PREFIX=ecasbot_%RELVER%
+) else (
+    set PREFIX=snapshot_%CI_HASH%
+)
+
 echo Removing previous build results...
 if exist results rd /S /Q results
 
@@ -32,13 +38,17 @@ echo Copying assets...
 copy /Y ..\..\config\ecasbot.json results\dist\ecasbot.json
 
 echo Signing binaries...
-"%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% results\dist\ecasbot.exe
+if [%CI_HASH%] == [] (
+    "%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% results\dist\ecasbot.exe
+)
 
 echo Compiling Installer...
 "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" inno\ecasbot.iss
 
 echo Signing built artifacts...
-"%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% results\ecasbot_%RELVER%.exe
+if [%CI_HASH%] == [] (
+    "%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% results\%PREFIX%_setup.exe
+)
 
 echo Removing temporary files and directories...
 del ecasbot.spec

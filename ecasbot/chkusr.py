e195c4a717edac4949602199a625bafbffec6cd6
Vitaly Zaitsev <vitaly@easycoding.org>
2022-05-07 14:31:44 +0200

    Updated base ID of newly registered user accounts.

--- ecasbot/chkusr.py
+++ ecasbot/chkusr.py
@@ -57,7 +57,7 @@ class CheckUsername:
         Check and score newly registered users.
         :return: Score result.
         """
-        return 50 if self.__userid > 900000000 else 0
+        return 50 if self.__userid > 3500000000 else 0
 
     def check_user_language(self) -> int:
         """

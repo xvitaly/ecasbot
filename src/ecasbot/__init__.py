e5e524c7c0289a66825bbce51662b48449d093f1
Vitaly Zaitsev <vitaly@easycoding.org>
2023-01-19 09:24:56 +0100

    Fixed `/unrestrict` command after Telegram Bot API changes.
    Closes #192.

--- src/ecasbot/__init__.py
+++ src/ecasbot/__init__.py
@@ -559,7 +559,9 @@ class ASBot:
             if message.reply_to_message:
                 self.__bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id,
                                                 can_send_messages=True, can_send_media_messages=True,
-                                                can_send_other_messages=True, can_add_web_page_previews=True)
+                                                can_send_other_messages=True, can_add_web_page_previews=True,
+                                                can_change_info=True, can_invite_users=True, can_pin_messages=True,
+                                                can_send_polls=True)
                 logmsg = self.__get_lm('as_aunres').format(message.from_user.first_name, message.from_user.id,
                                                            message.reply_to_message.from_user.first_name,
                                                            message.reply_to_message.from_user.id, message.chat.id,

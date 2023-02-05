c31c72a434188b2fed90e23856c7eec26b1f1d54
Vitaly Zaitsev <vitaly@easycoding.org>
2023-02-05 15:12:01 +0100

    Switched handle_unrestrict() to the new ChatPermissions object.

--- src/ecasbot/__init__.py
+++ src/ecasbot/__init__.py
@@ -598,11 +598,16 @@ class ASBot:
         """
         try:
             if message.reply_to_message:
+                unbanperm = telebot.types.ChatPermissions(can_send_messages=True, can_send_media_messages=True,
+                                                          can_send_audios=True, can_send_documents=True,
+                                                          can_send_photos=True, can_send_videos=True,
+                                                          can_send_video_notes=True, can_send_voice_notes=True,
+                                                          can_send_polls=True, can_send_other_messages=True,
+                                                          can_add_web_page_previews=True, can_change_info=True,
+                                                          can_invite_users=True, can_pin_messages=True,
+                                                          can_manage_topics=True)
                 self.__bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id,
-                                                can_send_messages=True, can_send_media_messages=True,
-                                                can_send_other_messages=True, can_add_web_page_previews=True,
-                                                can_change_info=True, can_invite_users=True, can_pin_messages=True,
-                                                can_send_polls=True)
+                                                permissions=unbanperm, use_independent_chat_permissions=True)
                 logmsg = self.__get_lm('as_aunres').format(message.from_user.first_name, message.from_user.id,
                                                            message.reply_to_message.from_user.first_name,
                                                            message.reply_to_message.from_user.id, message.chat.id,
